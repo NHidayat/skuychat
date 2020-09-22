035b90a959277b15915ddda906ade116081e6764
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-09-23 02:55:30 +0800

    Create: Login page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
-
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap');
+
+body {
+  background: #E5E5E5 !important;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: Rubik, sans-serif;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-container>
+      <div class="auth-wrapper">
+        <div class="auth-header">
+          <span>Login</span>
+        </div>
+        <div class="auth-body">
+          <span>Hi, Welcome back!</span>
+          <div class="auth-form">
+            <div class="input-g">
+              <label for="">Email</label>
+              <input type="email" />
+            </div>
+            <div class="input-g">
+              <label for="">Password</label>
+              <input type="password" />
+            </div>
+          </div>
+          <div class="text-right">
+            <router-link class="text-link primary" to="/forgot-password">Forgot password?</router-link>
+          </div>
+          <div class="auth-footer">
+            <button class="primary">Login</button>
+            <div class="text-center or">
+              <span>Login With</span>
+            </div>
+            <button class="primary-outline"><img src="../../assets/google-icon.png" alt=""> Login</button>
+            <div class="text-footer">
+              <span>Don’t have an account?</span>
+              <router-link class="text-link primary" to="/register"> Sign Up</router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </b-container>
+  </div>
+</template>
+<style src="../../assets/style/auth.css" scoped></style>
